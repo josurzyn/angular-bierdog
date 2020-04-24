3806efa6b803ca66897389c0a65225781b2a8886
Jo Surzyn <josurzyn@gmail.com>
2020-04-24 10:48:07 +0200

    test#19: fixes broken unit tests

--- src/app/components/filters/filters.component.spec.ts
+++ src/app/components/filters/filters.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FiltersComponent } from './filters.component';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
@@ -8,9 +13,15 @@ describe('FiltersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FiltersComponent ]
-    })
-    .compileComponents();
+      declarations: [FiltersComponent],
+      imports: [
+        FormsModule,
+        MatSliderModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/containers/biers/biers.component.spec.ts
+++ src/app/containers/biers/biers.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BiersComponent } from './biers.component';
+import { FiltersComponent } from '../../components/filters/filters.component';
+import { BierDetailComponent } from '../../components/bier-detail/bier-detail/bier-detail.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('BiersComponent', () => {
   let component: BiersComponent;
@@ -8,9 +19,19 @@ describe('BiersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BiersComponent ]
-    })
-    .compileComponents();
+      declarations: [BiersComponent, FiltersComponent, BierDetailComponent],
+      imports: [
+        MatProgressSpinnerModule,
+        MatSliderModule,
+        MatButtonModule,
+        MatCardModule,
+        MatSelectModule,
+        MatInputModule,
+        FormsModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
